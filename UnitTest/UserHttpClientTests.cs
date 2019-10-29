d2557c9811c19d3393bfc3f980e3a336b2e2be55
Balázs Keresztúri <19balazs86@gmail.com>
2019-10-29 21:57:22 +0000

    Small refactoring in UnitTest using StreamContent

--- UnitTest/UserHttpClientTests.cs
+++ UnitTest/UserHttpClientTests.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Mime;
 using System.Text;
 using System.Threading;
@@ -43,6 +45,8 @@ namespace UnitTest
           "SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
         .ReturnsAsync(createResponse(content: users))
         .Verifiable();
+
+      // The MemoryStream (in createResponse) will be closed after the GetUsersAsync due to response?.Dispose().
 
       // Act
       IEnumerable<string> response = await SUT.GetUsersAsync();
@@ -85,22 +89,54 @@ namespace UnitTest
       await Assert.ThrowsAsync<ServiceException>(() => SUT.GetUsersAsync());
     }
 
+    public void Dispose() => _httpClient.Dispose();
+
+    #region Helper methods
+
+    private static HttpResponseMessage createResponse(string content, HttpStatusCode statusCode = HttpStatusCode.OK)
+      => createResponse(new StringContent(content, Encoding.UTF8, MediaTypeNames.Application.Json), statusCode);
+
+    private static HttpResponseMessage createResponse(object content, HttpStatusCode statusCode = HttpStatusCode.OK)
+      => createResponse(createHttpStreamContent(content), statusCode);
+
     private static HttpResponseMessage createResponse(
-      string content = "",
+      HttpContent content,
       HttpStatusCode statusCode = HttpStatusCode.OK)
     {
       return new HttpResponseMessage
       {
-        StatusCode = statusCode,
-        Content    = new StringContent(content, Encoding.UTF8, MediaTypeNames.Application.Json)
+        Content    = content,
+        StatusCode = statusCode
       };
     }
 
-    private static HttpResponseMessage createResponse(object content, HttpStatusCode statusCode = HttpStatusCode.OK)
-      => createResponse(JsonConvert.SerializeObject(content), statusCode);
+    private static HttpContent createHttpStreamContent(object content)
+    {
+      var memoryStream = new MemoryStream();
 
-    public void Dispose() => _httpClient.Dispose();
+      serializeJsonIntoStream(content, memoryStream);
 
-    // And so on...
+      memoryStream.Seek(0, SeekOrigin.Begin);
+
+      HttpContent httpContent         = new StreamContent(memoryStream);
+      httpContent.Headers.ContentType = new MediaTypeHeaderValue(MediaTypeNames.Application.Json);
+
+      return httpContent;
+    }
+
+    private static void serializeJsonIntoStream(object value, Stream stream)
+    {
+      using var streamWriter   = new StreamWriter(stream, Encoding.UTF8, 1024, true);
+      using var jsonTextWriter = new JsonTextWriter(streamWriter);
+
+      new JsonSerializer().Serialize(jsonTextWriter, value);
+
+      jsonTextWriter.Flush();
+    }
+
+    // Efficient post calls with HttpClient and JSON.NET
+    // https://johnthiriet.com/efficient-post-calls
+
+    #endregion
   }
 }
